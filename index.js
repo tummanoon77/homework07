595da1a98b947c06b19ebfe993c8f3b0b33525e1
Tummanoon Sripechpong <62575162+tummanoon77@users.noreply.github.com>
2020-04-24 01:33:00 -0400

    prompt and axios try

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const questions = [
 
-];
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
-
-init();
+inquirer
+  .prompt([
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+      }, 
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your URL?",
+        name: "URL"
+      }, 
+    {
+    type: "input", 
+    message: "Enter your GitHub username",
+    name: "username"
+  }])
+  
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    axios
+  .get(queryUrl)
+  .then(resp=>  {
+console.log (resp)
+  })
+  })
